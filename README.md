855dc24592ba16418f24691d42c0f0e3f2703ee1
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2026-02-12 21:24:46 -0500

    first step

---
+++ README.md
@@ -0,0 +1,5 @@
+# `iai`
+
+`iai` (pronounced aye-aye) stands for ian's ai. a coding agent written by me, ian.
+
+The goal is to write an agent from scratch that will become self-improving over time. The end result thereby to
--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 def main():
-    print("Hello from iai!")
+    from openai import OpenAI
+    from dotenv import load_dotenv
+    import os
+
+    load_dotenv()
+
+    api_key = os.environ["OPENROUTER_API_KEY"]
+    # print(api_key)
+
+    client = OpenAI(base_url="https://openrouter.ai/api/v1", api_key=api_key)
+
+    completion = client.chat.completions.create(
+        model="mistralai/codestral-2508",
+        messages=[
+            {
+                "role": "user",
+                "content": "I am building a coding agent from scratch in python this is my first query. I am using the openai library via openrouter. I have implemented no tool yet. What would you advise I make first? ",
+            }
+        ],
+    )
+    print(completion.choices[0].message.content)
+    with open("output.md", "w") as f:
+        f.write(completion.choices[0].message.content)
 
 
 if __name__ == "__main__":
